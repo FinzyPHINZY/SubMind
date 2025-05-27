0038fc3d16d6dca256e73502112c6194215059d2
Boluwatife Adeyemi <finzyphinzy@gmail.com>
2025-05-27 16:15:14 +0100

    Set up Express server with a welcome route and updated package dependencies for ESLint and TypeScript support.

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
-console.log('hello world');
+import express from 'express';
+import type { Request, Response } from 'express';
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+  res.status(200).send('Welcome to SubMind! Your subscription tracker API');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}...betta go catch it`);
+});
